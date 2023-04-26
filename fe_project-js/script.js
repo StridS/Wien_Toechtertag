c92d49b291e72bf191b4e0479127a610e54ae8ff
Astrid <aschwarz.mmt-b2018@fh-salzburg.ac.at>
2023-04-26 17:52:57 +0200

    Copy html and css from js proj

--- fe_project-js/script.js
+++ fe_project-js/script.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const messageButton = document.getElementById('message-button');
+    const sendButton = document.getElementById('send-button');
+    const cancelButton = document.getElementById('cancel-button');
     const messageDialog = document.getElementById('dialog');
+    const messageInput = document.getElementById('message-input');
+    
     console.log(messageDialog);
 
     messageButton.addEventListener('click', () => {
         messageDialog.showModal();
+    })
+
+    sendButton.addEventListener('click', () => {
+        console.log(messageInput.value.length);
     })
 })
